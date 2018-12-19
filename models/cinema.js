20d720d2d262dc47e3e16e949afcd27bcaf37b48
RobinBailey84 <robinbailey52@hotmail.com>
2018-12-19 14:30:45 +0000

    initial commit

---
+++ models/cinema.js
@@ -0,0 +1,19 @@
+const Cinema = function (films) {
+  this.films = films;
+};
+
+Cinema.prototype.listOfTitles = function (){
+  const result = this.films.map((film) => {
+    return film.title
+  })
+  return result;
+};
+
+Cinema.prototype.findByTitle = function (title){
+  const result = this.films.find((film) => {
+    return film.title === title;
+  })
+  return result;
+}
+
+module.exports = Cinema;
